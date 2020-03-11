dcff460c7231fc426ef89f6edeed167384bbce52
rogerlorelli663 <roger.lorelli663@myci.csuci.edu>
2020-03-11 13:46:44 -0700

    Saving new files before synch

---
+++ Assets/Scripts/Card/CardManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    private enum CardType
+    {
+        NO_TYPE = 0,
+        MELEE = 1,
+        RANGED = 2,
+        SIEGE = 3,
+        WEATHER = 4,
+        EFFECT = 5, 
+        LEADER = 6,
+    }
+
+    [SerializeField] private CardType type = CardType.NO_TYPE;
+    private int cardType;
+    private string cardName;
+    private int power;
+
+    public int getCardType()
+    {
+        return cardType;
+    }
+
+    public string getName()
+    {
+        return cardName;
+    }
+
+    public void setCardType(int cardType)
+    {
+        this.cardType = cardType;
+    }
+
+    public void setName(string cardName)
+    {
+        this.cardName = cardName;
+    }
+
+    public int getPower()
+    {
+        return this.power;
+    }
+
+    public void setPower(int power)
+    {
+        this.power = power;
+    }
+}
