7878d8e64534ac5e42f01330812ccf964f0e5f32
MaxwellLight <maxwell.light238@myci.csuci.edu>
2020-03-13 12:25:04 -0700

    Added Card Execution

---
+++ Assets/Scripts/Card Execution Scripts/CardExecutionDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardExecutionDisplay : MonoBehaviour
+{
+
+    private CardExecutionManager cem;
+    [SerializeField] private float scale = 1;
+
+    void Start()
+    {
+        cem = GetComponent<CardExecutionManager>();
+    }
+
+    void Update()
+    {
+        GameObject cardDisplay = cem.GetDisplayCard();
+        if(cardDisplay != null)
+        {
+            cardDisplay.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+            cardDisplay.transform.localScale = new Vector2(scale, scale);
+        }
+    }
+}
---
+++ Assets/Scripts/Card Execution Scripts/CardExecutionManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardExecutionManager : MonoBehaviour
+{
+
+    private GameObject card = null;
+
+    private GameObject cardDisplay = null;
+
+    public GameObject GetCard()
+    {
+        return card;
+    }
+
+    public GameObject GetDisplayCard()
+    {
+        return cardDisplay;
+    }
+
+    public bool HasCard()
+    {
+        if(card != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void SetCardForPlay(GameObject card)
+    {
+        if(this.card == null)
+        {
+            this.card = card;
+            cardDisplay = Instantiate(this.card);
+            cardDisplay.transform.SetParent(gameObject.transform);
+            foreach(Transform child in this.card.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void removeCard()
+    {
+        foreach (Transform child in card.transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+        card = null;
+        Destroy(cardDisplay);
+    }
+
+}
