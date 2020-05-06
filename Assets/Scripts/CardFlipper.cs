c31339791a20b3589f099b1b3aeed1ee3b973031
kecottolazo <cottokv@gmail.com>
2020-05-06 13:39:36 -0700

    Unfinished Special Abilities Page, Updated Aesthetic of Tutorial

---
+++ Assets/Scripts/CardFlipper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardFlipper : MonoBehaviour
+{
+    public void Flip()
+    {
+        foreach (Transform child in gameObject.transform)
+        {
+            if (child.name == "BackSide")
+            {
+                if(child.gameObject.activeSelf)
+                {
+                    child.gameObject.SetActive(false);
+                    return;
+                }
+                else
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainManu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene("Remodeled2");
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        //SceneManager.LoadScene("Remodeled2");
+    }
+
+    public void TutorialtoMainMenu()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GametoMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        //SceneManager.LoadScene("Remodeled2");
+    }
+
+    public void GametoTutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene("Remodeled2");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
